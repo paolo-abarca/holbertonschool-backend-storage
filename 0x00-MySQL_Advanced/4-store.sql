0eddd76b07402a7b69b546ebd729a54ffeb9db6d
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-15 10:43:59 -0500

    Adding 4-store.sql file Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order --
+-- Quantity in the table items can be negative --
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders FOR EACH ROW
+UPDATE items
+SET items.quantity = items.quantity - NEW.number
+WHERE items.name = NEW.item_name;
