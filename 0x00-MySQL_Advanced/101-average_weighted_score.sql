959f06fc76c748286b45137875827b9ad8e05448
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-15 15:46:12 -0500

    Fixing 101-average_weighted_score.sql Write a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -5,9 +5,9 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
     UPDATE users
     SET average_score = (
-	SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+	SELECT SUM(score * weight) / SUM(weight)
         FROM corrections INNER JOIN
-        projects ON projects.id = corrections.project_id
-        WHERE corrections.user_id = user_id);
+        projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = users.id);
 END $$
 DELIMITER ;
